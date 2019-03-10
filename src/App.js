be39701b3c740a8f5a1b677bbc64e0c1e00ec111
André Gonçalves <andre.goncalves57@gmail.com>
2019-03-09 23:11:23 -0300

    [BM-0003] - Solve configuration problems and added API call

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { fetchIngredients, fetchRecipes } from './services/api';
 
-const App = () => <div className="App" />;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ingredients: undefined,
+      recipes: undefined,
+    };
+  }
+
+  componentDidMount() {
+    fetchIngredients().then(res => this.setState({ ingredients: res.data }));
+    fetchRecipes().then(res => this.setState({ recipes: res.data }));
+  }
+
+  render() {
+    return <div className="app" />;
+  }
+}
 
 export default App;
