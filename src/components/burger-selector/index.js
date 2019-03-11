f0eb519036a0550615c4ce95b7141edde8c4e26f
André Gonçalves <andre.goncalves57@gmail.com>
2019-03-11 08:20:14 -0300

    [BM-0004] - Added burger selector and burger item component

--- src/components/burger-selector/index.js
+++ src/components/burger-selector/index.js
@@ -1,17 +1,26 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
 import { Select } from './styles';
+import { fetchRecipes } from '../../services/api';
+import BurgerItem from './list-item-burger';
 
 class Selector extends Component {
   constructor(props) {
     super(props);
     this.state = {
       title: this.props.title,
+      recipes: undefined,
     };
   }
 
+  componentDidMount() {
+    fetchRecipes().then(res => this.setState({ recipes: res.data }));
+  }
+
   render() {
-    return (
+    window.console.log(this.props.type);
+    return this.state.recipes ? (
       <Select className="container">
         <div className="row">
           <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 collapse-header">
@@ -30,11 +39,17 @@ class Selector extends Component {
         </div>
         <div className="row collapse selector-body" id={`selector-body${this.props.index}`}>
           <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2">
-            <ul className="list-group" />
+            <ul className="list-group">
+              {this.props.type === 'default'
+                ? this.state.recipes.map(recipe => (
+                  <BurgerItem title={recipe.name} ingredients={recipe.ingredientsIds} />
+                ))
+                : null}
+            </ul>
           </div>
         </div>
       </Select>
-    );
+    ) : null;
   }
 }
 
